8f2c1e67cdb31f65c407b18c06222f2c6a442d97
cccccroge <pooopooo543@gmail.com>
2020-07-04 21:44:10 +0800

    Finish Variable

--- test/test.js
+++ test/test.js
@@ -1,20 +1,16 @@
-function loop(n){}
-function logEvenOrOdd(n){}
+'use strict';
 
-function loop(n){
-    if (n>1) {
-      logEvenOrOdd(--n)
-    }
-  }
-  
-function logEvenOrOdd(n) {
-console.log(n, n % 2 ? 'Odd' : 'Even')
-loop(n)
-}
+let a = String(NaN);      // "NaN"
+let b = "Okay" + 3;       // "Okay3"
+let c = "Okay" + 3 + NaN;  // "Okay3NaN"
+let d = 3 + NaN;           // NaN
 
-loop(10)
+let e = "6" - "2";         // 4
+let f = "6" + "2";         // "62"
+let g = +"  \t 123  \n"; // 123
+let h = +" 12 3  ";        // NaN
+let i = +"\t \n";        // 0
 
-
-console.log(a);
-var a = 5;
-
+let j = Boolean(undefined) || Boolean(null) || Boolean(NaN);  // false
+let k = Boolean(""); // false
+let l = Boolean(" ") && Boolean("0");  // true
